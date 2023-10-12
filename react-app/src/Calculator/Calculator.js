a935e4776801858737b00b3a14d51443d49c21a7
kevtool <kevinlim9171@gmail.com>
2023-10-12 23:40:04 +0800

    https

--- react-app/src/Calculator/Calculator.js
+++ react-app/src/Calculator/Calculator.js
@@ -68,7 +68,7 @@ function Calculator(props) {
         argStr = argStr.map((str) => str).join('');
         console.log(argStr)
 
-        fetch("http://kevtool.pythonanywhere.com/calculate?str=" + argStr, {
+        fetch("https://kevtool.pythonanywhere.com/calculate?str=" + argStr, {
             method: "GET",
         }).then((res) =>
             res.json().then((data) => {
