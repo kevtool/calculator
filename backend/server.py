17096a83e45ee91ed2d54cfb99ca031d349da1d3
kevtool <kevinlim9171@gmail.com>
2023-10-12 21:28:57 +0800

    test 8080

--- backend/server.py
+++ backend/server.py
@@ -23,6 +23,7 @@ def calculate():
 # Running app
 if __name__ == '__main__': 
     
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    
     # app.run(debug=True)
-    app.run()
-    # app.run(host='0.0.0.0', port=5000)
